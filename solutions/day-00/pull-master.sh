2c631dc4b8b88dd1f54e590a2fcb4060bfab5f54
serhat-io <zero4057@hotmail.de>
2024-08-24 23:25:36 +0200

    added script to auto pull master

---
+++ solutions/day-00/pull-master.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git checkout master
+git remote -v
+git remote add upstream git@github.com:Asabeneh/30-Days-Of-React.git
+git remote -v
+git fetch upstream
+git merge upstream/master master
+git push origin master
+git checkout exercise-solutions
+git fetch upstream
+git merge upstream/master master
+git push origin master
